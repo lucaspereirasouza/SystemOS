1a044e82e9b79f2b55fba17068c08ee3ae8b53bf
lucaspereirasouza <lucaspereiradesouza2163@outlook.com>
2023-10-12 20:06:20 -0300

    feat: added CPF in relatorios

--- src/view/Relatorios.java
+++ src/view/Relatorios.java
@@ -91,26 +91,21 @@ public class Relatorios extends JDialog {
 	 */
 	
 	private void relatorioClientes() {
-		//instanciar um objeto para construir a página pdf
 		Document document = new Document();
-		//configurar como A4 e modo paisagem
-		//document.setPageSize(PageSize.A4.rotate());
-		//gerar o documento pdf
 		try {
-			//criar um documento em branco (pdf) de nome clientes.pdf
+			
 			PdfWriter.getInstance(document, new FileOutputStream("clientes.pdf"));
-			//abrir o documento (formatar e inserir o conteúdo)
+			
 			document.open();
-			//adicionar a data atual
+			
 			Date dataRelatorio = new Date();
 			DateFormat formatador = DateFormat.getDateInstance(DateFormat.FULL);
 			document.add(new Paragraph(formatador.format(dataRelatorio)));
-			//adicionar um páragrafo
+			
 			document.add(new Paragraph("Clientes:"));
 			document.add(new Paragraph(" ")); //pular uma linha
-			//----------------------------------------------------------
-			//query (instrução sql para gerar o relatório de clientes)
-			String readClientes = "select nome,fone,cep from clientes order by nome";
+			
+			String readClientes = "select nome,fone,cep,cpf from clientes order by nome";
 			try {
 				//abrir a conexão com o banco
 				con = dao.conectar();
@@ -120,38 +115,47 @@ public class Relatorios extends JDialog {
 				rs = pst.executeQuery();
 				//atenção uso do while para trazer todos os clientes
 				// Criar uma tabela de duas colunas usando o framework(itextPDF)
-				PdfPTable tabela = new PdfPTable(3); //(2) número de colunas
-				// Criar o cabeçalho da tabela
+				PdfPTable tabela = new PdfPTable(4);
+				
 				PdfPCell col1 = new PdfPCell(new Paragraph("Cliente"));
 				PdfPCell col2 = new PdfPCell(new Paragraph("Telefone"));
-				PdfPCell col3 = new PdfPCell(new Paragraph("Cpf"));
+				PdfPCell col3 = new PdfPCell(new Paragraph("Cep"));
+				PdfPCell col4 = new PdfPCell(new Paragraph("Cpf"));
+				
 				tabela.addCell(col1);
 				tabela.addCell(col2);
 				tabela.addCell(col3);
+				tabela.addCell(col4);
+				
 				while (rs.next()) {
 					//popular a tabela
 					tabela.addCell(rs.getString(1));
 					tabela.addCell(rs.getString(2));
 					tabela.addCell(rs.getString(3));
+					tabela.addCell(rs.getString(4));
 				}				
 				//adicionar a tabela ao documento pdf
 				document.add(tabela);
 				//fechar a conexão com o banco
 				con.close();
+				
+				Image imagem = Image.getInstance(Relatorios.class.getResource("/img/ConsoleIcon.png"));
+				imagem.scaleToFit(128,128);
+				imagem.setAbsolutePosition(350,750);
+				document.add(imagem);
+				
 			} catch (Exception e) {
-				System.out.println(e);
+				e.printStackTrace();
 			}
 		} catch (Exception e) {
-			System.out.println(e);
+			e.printStackTrace();
 		}
-		//fechar o documento (pronto para "impressão" (exibir o pdf))
 		document.close();
-		//Abrir o desktop do sistema operacional e usar o leitor padrão
-		//de pdf para exibir o documento
+		
 		try {
 			Desktop.getDesktop().open(new File("clientes.pdf"));
 		} catch (Exception e) {
-			System.out.println(e);
+			e.printStackTrace();
 		}		
 	}
 	//IMPRESSÃO DE RELATÓRIOS
@@ -227,10 +231,10 @@ public class Relatorios extends JDialog {
 					//fechar a conexão com o banco
 					con.close();
 				} catch (Exception e) {
-					System.out.println(e);
+					e.printStackTrace();
 				}
 			} catch (Exception e) {
-				System.out.println(e);
+				e.printStackTrace();
 			}
 			//fechar o documento (pronto para "impressão" (exibir o pdf))
 			document.close();
@@ -239,7 +243,7 @@ public class Relatorios extends JDialog {
 			try {
 				Desktop.getDesktop().open(new File("servicos.pdf"));
 			} catch (Exception e) {
-				System.out.println(e);
+				e.printStackTrace();
 			}		
 		}// FIM DO RELATÓRIO CLIENTES
 }//fim do construtor
