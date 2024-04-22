097d4712b4891c60a8dab5013a2b365273001292
Lucas Pereira de Souza <lucaspereiradesouza2163@outlook.com>
2024-04-22 18:42:26 -0300

    removed comment

--- src/model/DAO.java
+++ src/model/DAO.java
@@ -6,14 +6,10 @@ import java.sql.DriverManager;
 public class DAO {
 	private String driver = "com.mysql.cj.jdbc.Driver";
 	private String url = "jdbc:mysql://127.0.0.1:3306/databaseos";
-//	private String url = "jdbc:mysql://10.26.45.241:3307/databaseos";
 	private String user = "root";
-	private String password = "123@changeme";
+	private String password = "root@passwd@changeme";
 	// Criação de um objeto para uso da classe Connection(JDBC)
 	private Connection con;
-	
-	
-	
 	/**
 	 * Método responsavel pela conexão jdbc no sistema
 	 * @return con
